a076b070a69e5238773f87c73752f9f5254f42c3
Methylase <methyl2007@yahoo.com>
2020-07-02 18:18:27 -0700

    update database.php
    i edited a database.php

--- config/database.php
+++ config/database.php
@@ -39,13 +39,27 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'registration'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'wanted1986'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],*/
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'dregister'),
+            'username' => env('DB_USERNAME', 'methyl2007'),
+            'password' => env('DB_PASSWORD', 'wanted1986.'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
